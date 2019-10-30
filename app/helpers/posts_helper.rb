60d10be428a228aecf21866a08c40c8317b61924
yuanchang <759337828@qq.com>
2019-10-30 10:18:56 +0000

    font helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,12 @@ module PostsHelper
   def render_post_description(post)
     simple_format(post.description)
   end
+
+  def render_job_status(job)
+    if job.is_hidden
+      content_tag(:span, "", :class => "fa fa-lock")
+    else
+      content_tag(:span, "", :class => "fa fa-globe")
+    end
+  end
 end
